520206e67a3548c9e92aa4f1a4ae32022f3e4a19
Anas Idrissi taouraghti <aidrissi@e1r8p1.1337.ma>
2020-12-13 14:30:02 +0100

    last

---
+++ get_next_line_bonus.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aidrissi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/10 20:21:27 by aidrissi          #+#    #+#             */
+/*   Updated: 2019/12/25 16:27:07 by aidrissi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+size_t	ft_strlen(const char *s);
+void	ft_bzero(void *s, size_t n);
+int		ft_chr(const char *s, char c);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *s1);
+int		ft_return(char **ch, int n, int fd, char **line);
+int		get_next_line(int fd, char **line);
+
+#endif
